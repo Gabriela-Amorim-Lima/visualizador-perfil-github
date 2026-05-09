8559f2a7b3641b896529a25427f0bcc38be3ea51
Gabriela Amorim <gabyamorimlima@hotmail.com>
2026-05-09 02:42:24 -0300

    feat: adicionou tela de carregamento e style

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+const btnSearch = document.getElementById('btn-search');
+const inputSearch = document.getElementById('input-search');
+const profileResults = document.querySelector('.profile-results')
+
+const base_URL = 'https://api.github.com';
+
+btnSearch.addEventListener('click', async() => {
+    const userName = inputSearch.value;
+    if(userName){
+        profileResults.innerHTML= `<p class="loading"> Carregando...</p>`;
+
+        try{
+        const response = await fetch (`${base_URL}/users/${userName}`);
+
+        if(!response.ok) {
+            alert ('Usuário não encontrado. Por favor verifique o nome do usuário novamente e tente novamente.');
+            profileResults.innerHTML= "";
+            return;
+        }
+        
+        const userData = await response.json();
+        console.log(userData);
+
+        profileResults.innerHTML = `
+        <div class="profile-card">
+    <img 
+        src="${userData.avatar_url}" 
+        alt="Avatar de ${userData.name}" 
+        class="profile-avatar"
+    />
+
+    <div class="profile-info">
+        <h2>${userData.name}</h2>
+        <p>${userData.bio || 'Não possui bio cadastrada 😢.'}</p>
+    </div>
+</div>`;
+      
+        }catch(error) {
+            console.error('Erro ao buscar o perfil do usuário:', error);
+            alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor tente novamente mais tarde')
+        }
+        
+        
+    }else {
+        alert('Por favor, digite um nome de usuário do GitHub.')
+    }
+});
+
